b557b7421850f03040daa951d1e54e5778236d4d
Dirane TAFEN <diranetafen@yahoo.com>
2021-11-16 12:50:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,15 +25,15 @@ data "aws_backup_vault" "existing" {
 
 resource "aws_backup_plan" "default" {
   count = local.plan_enabled ? 1 : 0
-  name  = var.plan_name_suffix == null ? module.this.id : format("%s_%s", module.this.id, var.plan_name_suffix)
+  name  = var.plan_name_suffix == null ? local.vault_name : format("%s_%s", local.vault_name, var.plan_name_suffix)
 
   rule {
-    rule_name                = module.this.id
+    rule_name                = local.vault_name
     target_vault_name        = join("", local.vault_enabled ? aws_backup_vault.default.*.name : data.aws_backup_vault.existing.*.name)
     schedule                 = var.schedule
     start_window             = var.start_window
     completion_window        = var.completion_window
-    recovery_point_tags      = module.this.tags
+    recovery_point_tags      = local.vault_name
     enable_continuous_backup = var.enable_continuous_backup
 
     dynamic "lifecycle" {
@@ -95,7 +95,7 @@ resource "aws_iam_role_policy_attachment" "default" {
 
 resource "aws_backup_selection" "default" {
   count        = local.plan_enabled ? 1 : 0
-  name         = var.vault_name
+  name         = local.vault_name
   iam_role_arn = join("", var.iam_role_enabled ? aws_iam_role.default.*.arn : data.aws_iam_role.existing.*.arn)
   plan_id      = join("", aws_backup_plan.default.*.id)
   resources    = var.backup_resources
